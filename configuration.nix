3eda19e0d1fd801fefde9abe26843cbfcce9dff2
Nathan Perez <me@nathanperez.dev>
2026-01-26 00:26:09 -0600

    bluetooth enable

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,11 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # bluetooth
+  hardware.xone.enable = true; 
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
